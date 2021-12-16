eba7cea81f1130d4174f2111ffde76e02df15c8f
Jerso Sando <jerso.sando@gmail.com>
2021-12-16 13:57:57 -0700

    problem four

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -49,7 +49,13 @@ let evensArr = []
 
 //Code Here
 
+for (i = 0; i < nums.length; i++){
+  if (nums [i] % 2 === 0){
+    evensArr.push(nums[i])
+  }
+}
 
+console.log(evensArr)
 
 ////////// PROBLEM 5 //////////
 
